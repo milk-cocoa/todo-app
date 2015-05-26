0dbc573c5c1bc10e9d924a7487619e63b6115edc
Shuhei Hiya <syuhei176@gmail.com>
2015-05-26 11:45:13 +0900

    v2

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 (function(global){
+    var milkcocoa = new MilkCocoa("vuei9dh5mu3.mlkcca.com");
+    var todoDataStore = milkcocoa.dataStore("todos");
+
 	global.onload = onload;
 	function onload() {
 		var new_content = document.getElementById("new_content");
 		var create_button = document.getElementById("create_btn");
 		var todos = document.getElementById("todos");
+
+		//1
+		todoDataStore.stream().sort('desc').size(20).next(function(err, todos) {
+			todos.forEach(function(todo) {
+				render_todo(todo.value);
+			});
+		});
+		//2
+		todoDataStore.on('push', function(pushed) {
+			render_todo(pushed.value);
+		});
+		//3
 		create_button.addEventListener("click", function(e) {
-			var element = document.createElement("div");
-			element.textContent = new_content.value;
-			todos.appendChild(element);
+			todoDataStore.push({
+				content : new_content.value
+			});
 			new_content.value = "";
 		});
+
+		function render_todo(todo) {
+			var element = document.createElement("div");
+			element.textContent = todo.content
+			todos.appendChild(element);
+		}
 	}
 }(window))
